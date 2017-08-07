6b9ae6dc25110969bc593c27775587d2710a1a81
Willian Custódio <wrafhael@gmail.comgit statusexitgit config --global user.email wrafhael@gmail.com>
2017-08-07 11:19:22 -0300

    testing

--- start/read_ledger.go
+++ start/read_ledger.go
@@ -3,20 +3,22 @@ package main
 import (
 	"errors"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"fmt"
 )
 
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-    logger.Debug("query function: ", function)
+	logger.Debug("query function: ", function)
+	fmt.Println(args)
 
 	if function == "get_volume" {
-		return t.get_volume(stub, args[0])
+		return t.get_volume(stub)
 	}
 
 	return nil, errors.New("Received unknown function invocation " + function)
 }
 
-func (t *SimpleChaincode) get_volume(stub shim.ChaincodeStubInterface, tracker_id string) ([]byte, error) {
-	bytes, err := stub.GetState(tracker_id)
+func (t *SimpleChaincode) get_volume(stub shim.ChaincodeStubInterface) ([]byte, error) {
+	bytes, err := stub.GetState()
 
 	return bytes, err
 }
